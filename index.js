bca74822fe218378a6704f4277793df87c8a7061
majaliju <majalijunyc@gmail.com>
2022-01-21 20:12:59 -0500

    commit tree 130b3150e0be75448bea381d8e7ad586bb02d77e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "Johnny",
+    shift: "daytime",
+    streetAddress: {
+        line1: "551",
+        line2: "Park Ave"
+    }
+}
+
+function updateEmployeeWithKeyAndValue(employee, key="nickname", value="Johnny Boy"){
+    return {...employee,
+    [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key="nickname", value="Johnny Rocket"){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key = "shift"){
+    // code passes the test but I don't feel it's the most proper way 
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
